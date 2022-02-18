a016f751731df45ca2e96dee8e247407571e9bef
Juan Alcantara <40488349@live.napier.ac.uk>
2022-02-18 09:38:34 +0000

    Remooved mongo db

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,16 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hi moon!");
+
+
+
+
     }
 }
